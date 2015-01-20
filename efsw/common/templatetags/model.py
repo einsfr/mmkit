ee5c113aef8d212dd4b281be313c6061846ad2e9
einsfr <einsfr@users.noreply.github.com>
2015-01-20 22:21:28 +0300

    Доработал verbose_name и field_verbose_name для работы со списками моделей, дописал тесты для verbose_name и подготовительной функции.

--- efsw/common/templatetags/model.py
+++ efsw/common/templatetags/model.py
@@ -1,29 +1,48 @@
 from django import template
+from django.db import models
 
 register = template.Library()
 
 # http://stackoverflow.com/questions/14496978/fields-verbose-name-in-templates
 
 
+def _prepare_instance(instance):
+    if isinstance(instance, models.Model):
+        obj = instance
+    elif hasattr(instance, '__iter__') and len(instance) > 0 and isinstance(instance[0], models.Model):
+        obj = instance[0]
+    else:
+        obj = None
+    return obj
+
+
 @register.simple_tag()
 def verbose_name(instance, plural=False):
-    if plural:
-        try:
-            result = instance._meta.verbose_name_plural
-        except:
-            result = ''
+    obj = _prepare_instance(instance)
+    if obj is not None:
+        if plural:
+            try:
+                result = obj._meta.verbose_name_plural
+            except:
+                result = ''
+        else:
+            try:
+                result = obj._meta.verbose_name
+            except:
+                result = ''
     else:
-        try:
-            result = instance._meta.verbose_name
-        except:
-            result = ''
+        result = ''
     return result
 
 
 @register.simple_tag()
 def field_verbose_name(instance, field_name):
-    #try:
-    result = instance._meta.get_field(field_name).verbose_name
-    #except:
-    #    result = ''
+    obj = _prepare_instance(instance)
+    if obj is not None:
+        try:
+            result = obj._meta.get_field(field_name).verbose_name
+        except:
+            result = ''
+    else:
+        result = ''
     return result
---
+++ efsw/common/tests.py
@@ -0,0 +1,52 @@
+from django.test import TestCase
+from django.db import models
+
+from efsw.common.templatetags import model
+
+
+class TemplateTagsTestCase(TestCase):
+
+    class TestModel(models.Model):
+
+        class Meta:
+            verbose_name='тестомодель'
+            verbose_name_plural = 'тестомодели'
+
+        name = models.CharField(
+            max_length=255,
+            verbose_name='тестовое имя'
+        )
+
+    class TestModelWithoutVerbose(models.Model):
+
+        name = models.CharField(
+            max_length=255
+        )
+
+    class NotModel():
+        pass
+
+    def test_prepare_instance(self):
+        instance = self.TestModel()
+        not_instance = self.NotModel()
+        self.assertEqual(model._prepare_instance(instance), instance)
+        self.assertEqual(model._prepare_instance([instance]), instance)
+        self.assertIsNone(model._prepare_instance(not_instance))
+        self.assertIsNone(model._prepare_instance([not_instance]))
+
+    def test_verbose_name(self):
+        not_instance = self.NotModel()
+        self.assertEqual(model.verbose_name(not_instance), '')
+        self.assertEqual(model.verbose_name(not_instance, False), '')
+        self.assertEqual(model.verbose_name(not_instance, True), '')
+        instance = self.TestModel()
+        self.assertEqual(model.verbose_name(instance), 'тестомодель')
+        self.assertEqual(model.verbose_name(instance, False), 'тестомодель')
+        self.assertEqual(model.verbose_name(instance, True), 'тестомодели')
+        instance_wo = self.TestModelWithoutVerbose()
+        self.assertEqual(model.verbose_name(instance_wo), 'test model without verbose')
+        self.assertEqual(model.verbose_name(instance_wo, False), 'test model without verbose')
+        self.assertEqual(model.verbose_name(instance_wo, True), 'test model without verboses')
+
+    def test_field_verbose_name(self):
+        pass  # TODO: закончить
