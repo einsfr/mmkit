cc9d677e9503242b427ce6683411d41757464ba5
einsfr <einsfr@users.noreply.github.com>
2015-02-10 15:37:11 +0300

    Настроена работа с Haystack+ElasticSearch

--- mmkit/conf/test.py
+++ mmkit/conf/test.py
@@ -13,6 +13,14 @@ DATABASES = {
     }
 }
 
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        'URL': 'http://127.0.0.1:9200',
+        'INDEX_NAME': 'mmkit',
+    }
+}
+
 SECRET_KEY = 'my_very_secret_test_key'
 
 EFSW_ARCH_STORAGE_ROOT = os.path.join(BASE_DIR, '_storage_test')
