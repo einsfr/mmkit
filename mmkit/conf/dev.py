34b7b5f6b9d9ca473025f39185c2bfc21f3aecab
einsfr <einsfr@users.noreply.github.com>
2015-02-16 15:08:39 +0300

    Изменена конфигурация - теперь всё, что относится к подключениям к БД и поиску, пути к папкам - всё перенесено в локальные конфигурационные файлы.

--- mmkit/conf/dev.py
+++ mmkit/conf/dev.py
@@ -6,23 +6,8 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 INSTALLED_APPS += ('debug_toolbar', )
 
-EFSW_ARCH_STORAGE_ROOT = os.path.join(BASE_DIR, '_storage_dev')
-
 EFSW_ARCH_SKIP_FS_OPS = False
-
-EFSW_ELASTIC_HOSTS = [
-    {'host': '127.0.0.1'},
-]
-
-EFSW_ELASTIC_OPTIONS = {}
 
 from mmkit.conf.dev_local import *
--- mmkit/conf/test.py
+++ mmkit/conf/test.py
@@ -6,23 +6,6 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-SECRET_KEY = 'my_very_secret_test_key'
-
-EFSW_ARCH_STORAGE_ROOT = os.path.join(BASE_DIR, '_storage_test')
-
 EFSW_ARCH_SKIP_FS_OPS = True # Не надо это трогать, иначе некоторые тесты могут не проходить. Где необходимо - этот параметр изменён прямо на месте
 
-EFSW_ELASTIC_HOSTS = [
-    {'host': '127.0.0.1'},
-]
-
-EFSW_ELASTIC_OPTIONS = {}
-
-EFSW_ELASTIC_INIT_MAPPINGS = ()
+from mmkit.conf.test_local import *
