9eca65829a8536a91f52582483c0771a5b98aaed
einsfr <einsfr@users.noreply.github.com>
2015-02-20 09:16:49 +0300

    Забыл про вид.

--- mmkit/urls.py
+++ mmkit/urls.py
@@ -5,7 +5,7 @@ from mmkit import views
 
 urlpatterns = patterns(
     '',
-    url(r'^/', views.home_page()),
+    url(r'^/', views.home_page),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^archive/', include('efsw.archive.urls', namespace='efsw.archive'))
 )
