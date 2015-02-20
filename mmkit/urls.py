7615b74f5fbd1a7bb9ce600e1c98255220379c8b
einsfr <einsfr@users.noreply.github.com>
2015-02-20 09:15:59 +0300

    Домашняя страница для всего приложения

--- mmkit/urls.py
+++ mmkit/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-                       url(r'^admin/', include(admin.site.urls)),
-                       url(r'^archive/', include('efsw.archive.urls', namespace='efsw.archive'))
-                       )
+from mmkit import views
+
+urlpatterns = patterns(
+    '',
+    url(r'^/', ),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^archive/', include('efsw.archive.urls', namespace='efsw.archive'))
+)
