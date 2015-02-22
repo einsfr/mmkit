4f95ddf7d82d96ca5de216d3726328ce947ffb73
einsfr <einsfr@users.noreply.github.com>
2015-02-22 14:13:24 +0300

    Добавлен выбор даты записи

--- efsw/archive/forms.py
+++ efsw/archive/forms.py
@@ -2,6 +2,7 @@ from django import forms
 
 from efsw.archive import models
 
+from efsw.common.datetime.period import DatePeriod
 
 class ItemCreateForm(forms.ModelForm):
     class Meta:
@@ -35,13 +36,17 @@ class ItemCategoryForm(forms.ModelForm):
 
 
 class ArchiveSearchForm(forms.Form):
-    ORDER_BY_CREATED_ASC = 'cra'
-    ORDER_BY_CREATED_DESC = 'crd'
+    ORDER_BY_CREATED_ASC = '1'
+    ORDER_BY_CREATED_DESC = '2'
 
     ORDER_CHOICES = {
         ORDER_BY_CREATED_ASC: 'По дате создания (по возрастанию)',
         ORDER_BY_CREATED_DESC: 'По дате создания (по убыванию)',
     }
+
+    DATE_PERIODS = DatePeriod.PERIODS_PAST_ONLY_WITH_TODAY
+
+    DATE_CHOICES = DatePeriod.get_periods_past_only()
 
     q = forms.CharField(
         min_length=3,
@@ -56,5 +61,10 @@ class ArchiveSearchForm(forms.Form):
     o = forms.ChoiceField(
         choices=[('', 'По релевантности')] + list(ORDER_CHOICES.items()),
         label='Порядок сортировки',
+        required=False,
+    )
+    p = forms.ChoiceField(
+        choices=[(x[0], str(x[1]).capitalize()) for x in DATE_CHOICES.items()],
+        label='Дата записи',
         required=False,
     )
