07c1e211e0d693fb4ee17b955d47da740248e50c
einsfr <einsfr@users.noreply.github.com>
2015-02-27 13:47:57 +0300

    Для модели ItemLog не нужны никакие разрешения - отключил создаваемые по-умолчанию.

--- efsw/archive/models.py
+++ efsw/archive/models.py
@@ -156,6 +156,7 @@ class ItemLog(models.Model):
     class Meta:
         verbose_name = 'запись'
         verbose_name_plural = 'записи'
+        default_permissions = ()
 
     ACTION_ADD = 'ADD'
     ACTION_UPDATE = 'UPD'
