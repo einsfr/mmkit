23e362f3a86978c794d204780a2e3bd36db9ee57
einsfr <einsfr@users.noreply.github.com>
2015-03-06 19:45:22 +0300

    Исправил используемые конфиги

--- manage.py
+++ manage.py
@@ -4,9 +4,9 @@ import sys
 
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == 'test':
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mmkit.conf.test")
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mmkit.conf.test_local")
     else:
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mmkit.conf.dev")
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mmkit.conf.dev_local")
 
     from django.core.management import execute_from_command_line
 
