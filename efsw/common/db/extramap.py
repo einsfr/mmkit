4f4cc1dee489d8245caa94e197a530cc80ee1ba4
einsfr <einsfr@users.noreply.github.com>
2015-03-13 20:00:42 +0300

    Продолжение работы над полем с дополнительными данными

--- efsw/common/db/extramap.py
+++ efsw/common/db/extramap.py
@@ -1,6 +1,7 @@
+from django.db import models
 
 
-class BaseExtraFieldsMapper():
+class BaseExtraFieldsMapper():  # TODO: mapping должен стать объектом, а не словарём
 
     def __init__(self):
         self.mapping = {}
@@ -12,6 +13,11 @@ class BaseExtraFieldsMapper():
         return field_name in self.get_mapping()
 
     def add(self, name, field_obj):
+        if not type(name) == str:
+            raise TypeError('Имя дополнительного поля должно быть строкой.')
+        if not isinstance(field_obj, models.Field):
+            raise TypeError('Параметр field_obj должен быть подклассов класса django.db.models.Field.')
         field_obj.null = True
+        field_obj.set_attributes_from_name(name)
         self.mapping[name] = field_obj
         return self
--- efsw/common/db/models.py
+++ efsw/common/db/models.py
@@ -2,14 +2,6 @@ from django.db import models
 from django.contrib.postgres import fields
 
 
-class ExtraDataField(fields.HStoreField):
-
-    def __init__(self, **kwargs):
-        kwargs['null'] = True
-        kwargs['editable'] = False
-        super().__init__(**kwargs)
-
-
 class AbstractExtraDataModel(models.Model):
     """
     Общий смысл получается примерно такой: extra_data хранит значения дополнительных полей данных и работает как обычное
@@ -21,20 +13,25 @@ class AbstractExtraDataModel(models.Model):
     отдельных полей в составное поле extra_data и за их отображение в виде виджетов формы. Ну и, конечно, за валидацию
     каждого отдельного поля, т.к. они все являются стандартными Django-полями - проблем с этим быть не должно вообще
     никаких.
-
-    МНЕ НИЧЕГО НЕ МЕШАЕТ СДЕЛАТЬ ЭТО ПОЛЕ В ВИДЕ ГОТОВОЙ МОДЕЛИ, У КОТОРОЙ ПРОСТО НЕ БУДЕТ СВОЕЙ ТАБЛИЦЫ
     """
 
     class Meta:
         abstract = True
 
-    extra_data = ExtraDataField()
+    extra_data = fields.HStoreField(
+        null=True,
+        editable=False
+    )
 
     def save(self, *args, **kwargs):
         # Если поле не определено mapper'ом - его при сохранении надо просто выкинуть, а валидации не будет всё равно
         if type(self.extra_data) == dict:
-            cleaned_extra_data = dict([(k, v) for k, v in self.extra_data.items() if self.extra_field_exists(k)])
-            self.extra_data = cleaned_extra_data  # TODO: надо ещё пройтись по всем полям и привести их к пригодному для сохранения в БД виду https://docs.djangoproject.com/en/1.8/ref/models/instances/#what-happens-when-you-save
+            cleaned_extra_data = {}
+            field_mapping = self.get_extra_fields_mapping()
+            for f_name, f_obj in self.extra_data.items():
+                if f_name in field_mapping:
+                    cleaned_extra_data[f_name] = field_mapping[f_name].value_to_string(self.extra_data)
+            self.extra_data = cleaned_extra_data
         else:
             self.extra_data = None
         super().save(*args, **kwargs)
@@ -43,6 +40,9 @@ class AbstractExtraDataModel(models.Model):
         raise NotImplementedError(
             'Метод get_extra_fields_mapper должен быть переопределён моделью перед использованием.'
         )
+
+    def get_extra_fields_mapping(self):
+        return self.get_extra_fields_mapper().get_mapping()
 
     def extra_field_exists(self, field_name):
         return self.get_extra_fields_mapper().field_exists(field_name)
