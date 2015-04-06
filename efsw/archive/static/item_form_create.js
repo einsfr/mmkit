27af153654b62be3e81efee8b64d7732f817f06c
einsfr <einsfr@users.noreply.github.com>
2015-04-06 10:15:46 +0300

    Поправил для удобства выбора года

--- efsw/archive/static/item_form_create.js
+++ efsw/archive/static/item_form_create.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-    $('#created_date_input').datepicker($.datepicker.regional['ru']);
+    $('#created_date_input').datepicker(
+        {
+            changeMonth: true,
+            changeYear: true,
+            yearRange: '1990:c'
+        },
+        $.datepicker.regional['ru']
+    );
 });
