c67358ec1b7a03a6b4ee4a46fae3bc32088f6c4e
einsfr <einsfr@users.noreply.github.com>
2015-04-08 18:44:57 +0300

    К именам добавлены и названия модулей - для надёжности

--- efsw/common/test/urls.py
+++ efsw/common/test/urls.py
@@ -9,5 +9,7 @@ class UrlsTestCase(TestCase):
             (view_name, args, url, view_func) = u
             print('Testing "{0}"... '.format(view_name), end='')
             self.assertEqual(url, urlresolvers.reverse(view_name, args=args))
-            self.assertEqual(view_func.__name__, urlresolvers.resolve(url).func.__name__)
+            resolved_url = urlresolvers.resolve(url)
+            self.assertEqual(view_func.__name__, resolved_url.func.__name__)
+            self.assertEqual(view_func.__module__, resolved_url.func.__module__)
             print('OK')
