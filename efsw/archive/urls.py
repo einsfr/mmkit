44b16a143bc9b0f2a131a77b91dfa404f82449dd
einsfr <einsfr@users.noreply.github.com>
2015-04-08 10:26:27 +0300

    Продолжение работы

--- efsw/archive/urls.py
+++ efsw/archive/urls.py
@@ -4,6 +4,103 @@ from django.contrib.auth.decorators import permission_required
 from efsw.archive import views
 
 
+single_item_patterns = [
+    # items/12/show/ Описание одного элемента
+    # ( Описание элемента )
+    url(
+        r'^show/$',
+        views.item_show,
+        name='show'
+    ),
+    # items/12/logs/list/ Сообщения о внесении изменения в элемент
+    # ( Журнал изменений элемента )
+    url(
+        r'^logs/list/$',
+        views.item_logs_list,
+        name='logs_list'
+    ),
+    # items/12/edit/ Редактирование существующего элемента - форма (GET)
+    # ( Редактирование элемента )
+    url(
+        r'^edit/$',
+        permission_required('archive.change_item')(views.item_edit),
+        name='edit'
+    ),
+    # items/12/update/ Редактирование существующего элемента - операция с БД (POST)
+    # ( - )
+    url(
+        r'^update/$',
+        permission_required('archive.change_item')(views.item_update),
+        name='update'
+    ),
+]
+
+item_patterns = [
+    # items/list/ Основной список элементов
+    # ( Список элементов )
+    url(
+        r'^list/$',
+        views.item_list,
+        name='list'
+    ),
+    # items/list/page/2/ Основной список элементов постранично
+    # ( Список элементов, страница 2 )
+    url(
+        r'^list/page/(?P<page>\d+)/$',
+        views.item_list,
+        name='list_page'
+    ),
+    # items/new/ Добавление нового элемента - страница с формой (GET)
+    # ( Добавление нового элемента )
+    url(
+        r'^new/$',
+        permission_required('archive.add_item')(views.item_new),
+        name='new'
+    ),
+    # items/create/ Добавление нового элемента - операция с БД (POST)
+    # ( - )
+    url(
+        r'^create/$',
+        permission_required('archive.add_item')(views.item_create),
+        name='create'
+    ),
+    # items/12/...
+    url(
+        r'^(?P<item_id>\d+)/',
+        include()  # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    ),
+    # ------------------------- JSON -------------------------
+    # items/includes/list/json/?id=12 Включаемые элементы (JSON)
+    # ( - )
+    url(
+        r'^includes/list/json/$',
+        views.item_includes_list_json,
+        name='includes_list_json'
+    ),
+    # items/includes/update/json/?id=12 Включаемые элементы - обновление (JSON, POST)
+    # ( - )
+    url(
+        r'^includes/update/json/$',
+        permission_required('archive.change_item')(views.item_includes_update_json),
+        name='includes_update_json'
+    ),
+    # items/locations/update/json/?id=12 Положение этого элемента в хранилищах (JSON)
+    # ( - )
+    url(
+        r'^locations/list/json/$',
+        views.item_locations_update_json,
+        name='locations_list_json'
+    ),
+    # items/locations/update/json/?id=12 Положение этого элемента в хранилищах - обновление (JSON, POST)
+    # ( - )
+    url(
+        r'^locations/update/json/$',
+        permission_required('archive.change_itemlocation')(views.item_locations_update_json),
+        name='locations_update_json'
+    ),
+]
+
+
 urlpatterns = [
     # search/... Поиск по архиву
     # ( Поиск по архиву )
@@ -16,99 +113,7 @@ urlpatterns = [
     # items/...
     url(
         r'^items/',
-        include([
-            # items/list/ Основной список элементов
-            # ( Список элементов )
-            url(
-                r'^list/$',
-                views.item_list,
-                name='list'
-            ),
-            # items/list/page/2/ Основной список элементов постранично
-            # ( Список элементов, страница 2 )
-            url(
-                r'^list/page/(?P<page>\d+)/$',
-                views.item_list,
-                name='list_page'
-            ),
-            # items/new/ Добавление нового элемента - страница с формой (GET)
-            # ( Добавление нового элемента )
-            url(
-                r'^new/$',
-                permission_required('archive.add_item')(views.item_new),
-                name='new'
-            ),
-            # items/create/ Добавление нового элемента - операция с БД (POST)
-            # ( - )
-            url(
-                r'^create/$',
-                permission_required('archive.add_item')(views.item_create),
-                name='create'
-            ),
-            # items/12/...
-            url(
-                r'^(?P<item_id>\d+)/',
-                include([
-                    # items/12/show/ Описание одного элемента
-                    # ( Описание элемента )
-                    url(
-                        r'^show/$',
-                        views.item_show,
-                        name='show'
-                    ),
-                    # items/12/logs/list/ Сообщения о внесении изменения в элемент
-                    # ( Журнал изменений элемента )
-                    url(
-                        r'^logs/list/$',
-                        views.item_logs_list,
-                        name='logs_list'
-                    ),
-                    # items/12/edit/ Редактирование существующего элемента - форма (GET)
-                    # ( Редактирование элемента )
-                    url(
-                        r'^edit/$',
-                        permission_required('archive.change_item')(views.item_edit),
-                        name='edit'
-                    ),
-                    # items/12/update/ Редактирование существующего элемента - операция с БД (POST)
-                    # ( - )
-                    url(
-                        r'^update/$',
-                        permission_required('archive.change_item')(views.item_update),
-                        name='update'
-                    ),
-                ])
-            ),
-            # ------------------------- JSON -------------------------
-            # items/includes/list/json/?id=12 Включаемые элементы (JSON)
-            # ( - )
-            url(
-                r'^includes/list/json/$',
-                views.item_includes_list_json,
-                name='includes_list_json'
-            ),
-            # items/includes/update/json/?id=12 Включаемые элементы - обновление (JSON, POST)
-            # ( - )
-            url(
-                r'^includes/update/json/$',
-                permission_required('archive.change_item')(views.item_includes_update_json),
-                name='includes_update_json'
-            ),
-            # items/locations/update/json/?id=12 Положение этого элемента в хранилищах (JSON)
-            # ( - )
-            url(
-                r'^locations/list/json/$',
-                views.item_locations_update_json,
-                name='locations_list_json'
-            ),
-            # items/locations/update/json/?id=12 Положение этого элемента в хранилищах - обновление (JSON, POST)
-            # ( - )
-            url(
-                r'^locations/update/json/$',
-                permission_required('archive.change_itemlocation')(views.item_locations_update_json),
-                name='locations_update_json'
-            ),
-        ], namespace='item')
+        include(, namespace='item')  # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     ),
     # ------------------------- ItemCategory -------------------------
     # categories/
