a409777581ef9c5687e95d845b0453eea1c37967
einsfr <einsfr@users.noreply.github.com>
2015-04-08 18:07:46 +0300

    Класс для автоматизации тестирования соответствия имён представлений, ссылок и функций

---
+++ efsw/common/test/urls.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+from django.core import urlresolvers
+
+
+class UrlsTestCase(TestCase):
+
+    def process_urls(self, url_list):
+        for u in url_list:
+            (view_name, args, url, view_func) = u
+            print('Testing "{0}"... '.format(view_name), end='')
+            self.assertEqual(url, urlresolvers.reverse(view_name, args=args))
+            self.assertEqual(view_func, urlresolvers.resolve(url).func)
+            print('OK')
