cde904797070436cf0a6f585da0672a8cd7c1a81
einsfr <einsfr@users.noreply.github.com>
2015-04-16 22:35:17 +0300

    В виджеты добавлены связи с данными

--- efsw/schedule/forms.py
+++ efsw/schedule/forms.py
@@ -15,35 +15,53 @@ class ProgramPositionControlForm(forms.Form):
     st_h = forms.IntegerField(
         label='Время начала (часы)',
         min_value=0,
-        max_value=23
+        max_value=23,
+        widget=forms.NumberInput(attrs={
+            'data-bind': 'value: pp().start_hours',
+        })
     )
 
     st_m = forms.IntegerField(
         label='Время начала (минуты)',
         min_value=0,
-        max_value=59
+        max_value=59,
+        widget=forms.NumberInput(attrs={
+            'data-bind': 'value: pp().start_minutes',
+        })
     )
 
     et_h = forms.IntegerField(
         label='Время окончания (часы)',
         min_value=0,
-        max_value=23
+        max_value=23,
+        widget=forms.NumberInput(attrs={
+            'data-bind': 'value: pp().end_hours',
+        })
     )
 
     et_m = forms.IntegerField(
         label='Время окончания (минуты)',
         min_value=0,
-        max_value=59
+        max_value=59,
+        widget=forms.NumberInput(attrs={
+            'data-bind': 'value: pp().end_minutes',
+        })
     )
 
     c = forms.CharField(
         max_length=32,
         label='Комментарий',
-        required=False
+        required=False,
+        widget=forms.TextInput(attrs={
+            'data-bind': 'value: pp().comment',
+        })
     )
 
     l = forms.BooleanField(
-        label='Заблокировано'
+        label='Заблокировано',
+        widget=forms.CheckboxInput(attrs={
+            'data-bind': 'value: pp().locked',
+        })
     )
 
     p = forms.ModelChoiceField(
