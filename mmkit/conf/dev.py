a8e06bb1896f377b97fc710a6912f3db5ae609db
einsfr <einsfr@users.noreply.github.com>
2015-04-23 14:03:47 +0300

    Для dev-окружения добавлено ведение логов SQL-запросов в консоли - из-за обилия JSON-запросов и ответов использовать debug-toolbar получается далеко не всегда

--- mmkit/conf/dev.py
+++ mmkit/conf/dev.py
@@ -11,3 +11,20 @@ EFSW_ARCH_SKIP_FS_OPS = False
 EFSW_ARCH_ITEM_LIST_PER_PAGE = 2
 
 EFSW_SCHED_PROGRAM_LIST_PER_PAGE = 2
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'DEBUG',
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        }
+    },
+}
