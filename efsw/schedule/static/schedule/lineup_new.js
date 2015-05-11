e33ccef43fbbc9f79f49d3232583a1ac9d9d6e4b
einsfr <einsfr@users.noreply.github.com>
2015-05-11 19:57:14 +0300

    Модель используется всегда - объединена с кодом основного модуля

--- efsw/schedule/static/schedule/lineup_new.js
+++ efsw/schedule/static/schedule/lineup_new.js
@@ -1,9 +1,47 @@
-define(['jquery', 'knockout', 'schedule/model_lineup_new'], function($, ko, model) {
+define(['jquery', 'knockout'], function($, ko) {
 
     return function(conf) {
         $(document).ready(function() {
-            ko.applyBindings(new model(conf.urls));
+            ko.applyBindings(new LineupNewViewModel(conf.urls));
         })
+    };
+
+    function LineupNewViewModel(urls) {
+        var self = this;
+        self.urls = urls;
+        self.form = $('#lineup_new_form');
+        self.errors_empty = {
+            name: '',
+            start_time: '',
+            end_time: '',
+            channel: ''
+        };
+        self.errors = ko.observable(self.errors_empty);
+
+        self.init = function() {
+            self.errors(self.errors_empty);
+        };
+
+        self.create_lineup = function() {
+            $.ajax(self.urls.lineup_create_json(), {
+                method: 'post',
+                data: self.form.serialize()
+            }).done(function(result) {
+                if (result.status == 'ok') {
+                    window.location.href = result.data;
+                } else {
+                    var errors = $.parseJSON(result.data.errors);
+                    for (var p in errors) {
+                        if (errors.hasOwnProperty(p)) {
+                            errors[p] = errors[p].map(function(e) { return e.message; }).join(' ');
+                        }
+                    }
+                    self.errors(errors);
+                }
+            }).fail(function(jqXHR, textStatus) {
+                alert('При создании возникла ошибка: ' + textStatus);
+            });
+        };
     }
 
 });
