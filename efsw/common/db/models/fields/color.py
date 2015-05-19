79a9c82a5e7fbe12f2ea26174c1d5ff4e1fd311c
einsfr <einsfr@users.noreply.github.com>
2015-05-19 18:17:28 +0300

    Изменена проверка строки

--- efsw/common/db/models/fields/color.py
+++ efsw/common/db/models/fields/color.py
@@ -14,5 +14,7 @@ class ColorField(CharField):
             raise ValidationError('Значение цвета должно начинаться с символа "#".')
         if len(value) != 7:
             raise ValidationError('Значение цвета должно иметь в длину ровно 7 символов.')
-        if any(map(lambda x: x not in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'], value[1:8])):
-            raise ValidationError('В значении цвета допустимы только цифры от 0 до 9 и буквы от "a" до "f".')
+        for c in value[1:8]:
+            if c not in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']:
+                raise ValidationError('В значении цвета допустимы только цифры от 0 до 9 и буквы от "a" до "f".')
+
