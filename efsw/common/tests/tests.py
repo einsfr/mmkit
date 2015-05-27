6fd0f0f7d0027805b58840700e8f4cd29bd5c139
einsfr <einsfr@users.noreply.github.com>
2015-05-27 15:16:11 +0300

    Убраны тэги field_verbose_...

--- efsw/common/tests/tests.py
+++ efsw/common/tests/tests.py
@@ -2,98 +2,15 @@ import os
 import datetime
 
 from django.test import TestCase, override_settings
-from django.db import models
 from django.core import paginator
 from django.conf import settings
 from django.db import connection
 
-from efsw.common.templatetags import model
 from efsw.common.templatetags import pagination
 from efsw.common import default_settings
 from efsw.common.search import elastic
 from efsw.common.management.commands import esinit
 from efsw.common.tests import models as test_models
-
-
-class ModelTagTestCase(TestCase):
-
-    class TestModel(models.Model):
-
-        class Meta:
-            verbose_name = 'тестомодель'
-            verbose_name_plural = 'тестомодели'
-
-        name = models.CharField(
-            max_length=255,
-            verbose_name='тестовое имя'
-        )
-
-    class TestModelWithoutVerbose(models.Model):
-
-        name = models.CharField(
-            max_length=255
-        )
-
-    class NotModel():
-        pass
-
-    def test_prepare_instance(self):
-        instance = self.TestModel()
-        not_instance = self.NotModel()
-        self.assertEqual(model._prepare_instance(instance), instance)
-        self.assertEqual(model._prepare_instance([instance]), instance)
-        self.assertIsNone(model._prepare_instance(not_instance))
-        self.assertIsNone(model._prepare_instance([not_instance]))
-        self.assertIsNone(model._prepare_instance([]))
-
-    def test_verbose_name(self):
-        not_instance = self.NotModel()
-        self.assertEqual(model.verbose_name(not_instance), '')
-        self.assertEqual(model.verbose_name(not_instance, False), '')
-        self.assertEqual(model.verbose_name(not_instance, True), '')
-        self.assertEqual(model.verbose_name(not_instance, False, False), '')
-        self.assertEqual(model.verbose_name(not_instance, False, True), '')
-        self.assertEqual(model.verbose_name(not_instance, True, False), '')
-        self.assertEqual(model.verbose_name(not_instance, True, True), '')
-        instance = self.TestModel()
-        self.assertEqual(model.verbose_name(instance), 'Тестомодель')
-        self.assertEqual(model.verbose_name(instance, False), 'Тестомодель')
-        self.assertEqual(model.verbose_name(instance, True), 'Тестомодели')
-        self.assertEqual(model.verbose_name(instance, False, False), 'тестомодель')
-        self.assertEqual(model.verbose_name(instance, False, True), 'Тестомодель')
-        self.assertEqual(model.verbose_name(instance, True, False), 'тестомодели')
-        self.assertEqual(model.verbose_name(instance, True, True), 'Тестомодели')
-        instance_wo = self.TestModelWithoutVerbose()
-        self.assertEqual(model.verbose_name(instance_wo), 'Test model without verbose')
-        self.assertEqual(model.verbose_name(instance_wo, False), 'Test model without verbose')
-        self.assertEqual(model.verbose_name(instance_wo, True), 'Test model without verboses')
-        self.assertEqual(model.verbose_name(instance_wo, False, False), 'test model without verbose')
-        self.assertEqual(model.verbose_name(instance_wo, False, True), 'Test model without verbose')
-        self.assertEqual(model.verbose_name(instance_wo, True, False), 'test model without verboses')
-        self.assertEqual(model.verbose_name(instance_wo, True, True), 'Test model without verboses')
-
-    def test_field_verbose_name(self):
-        not_instance = self.NotModel()
-        self.assertEqual(model.field_verbose_name(not_instance, 'name'), '')
-        self.assertEqual(model.field_verbose_name(not_instance, 'name', False), '')
-        self.assertEqual(model.field_verbose_name(not_instance, 'name', True), '')
-        self.assertEqual(model.field_verbose_name(not_instance, 'non-exist'), '')
-        self.assertEqual(model.field_verbose_name(not_instance, 'non-exist', False), '')
-        self.assertEqual(model.field_verbose_name(not_instance, 'non-exist', True), '')
-        instance = self.TestModel()
-        self.assertEqual(model.field_verbose_name(instance, 'name'), 'Тестовое имя')
-        self.assertEqual(model.field_verbose_name(instance, 'name', False), 'тестовое имя')
-        self.assertEqual(model.field_verbose_name(instance, 'name', True), 'Тестовое имя')
-        self.assertEqual(model.field_verbose_name(instance, 'non-exist'), '')
-        self.assertEqual(model.field_verbose_name(instance, 'non-exist', False), '')
-        self.assertEqual(model.field_verbose_name(instance, 'non-exist', True), '')
-        instance_wo = self.TestModelWithoutVerbose()
-        self.assertEqual(model.field_verbose_name(instance_wo, 'name'), 'Name')
-        self.assertEqual(model.field_verbose_name(instance_wo, 'name', False), 'name')
-        self.assertEqual(model.field_verbose_name(instance_wo, 'name', True), 'Name')
-        self.assertEqual(model.field_verbose_name(instance_wo, 'non-exist'), '')
-        self.assertEqual(model.field_verbose_name(instance_wo, 'non-exist', False), '')
-        self.assertEqual(model.field_verbose_name(instance_wo, 'non-exist', True), '')
 
 
 @override_settings(ROOT_URLCONF='efsw.common.tests.tests_urls')
