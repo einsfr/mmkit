8c7e1c681fa079da50ea4c37a844bb11024b62f8
einsfr <einsfr@users.noreply.github.com>
2015-05-28 19:16:48 +0300

    Добавлена возможность установить расширенное значение статуса в дополнительном поле ответа

--- efsw/common/http/response.py
+++ efsw/common/http/response.py
@@ -6,19 +6,20 @@ class JsonWithStatusResponse(JsonResponse):
     STATUS_OK = 'ok'
     STATUS_ERROR = 'err'
 
-    def __init__(self, data='', json_status=STATUS_OK, **kwargs):
+    def __init__(self, data='', json_status=STATUS_OK, status_ext=None, **kwargs):
         super().__init__(
             {
                 'status': json_status,
+                'status_ext': status_ext if status_ext is not None else '',
                 'data': data,
             },
             **kwargs
         )
 
     @classmethod
-    def ok(cls, data='', **kwargs):
-        return cls(data, cls.STATUS_OK, **kwargs)
+    def ok(cls, data='', status_ext=None, **kwargs):
+        return cls(data, cls.STATUS_OK, status_ext, **kwargs)
 
     @classmethod
-    def error(cls, data='', **kwargs):
-        return cls(data, cls.STATUS_ERROR, **kwargs)
+    def error(cls, data='', status_ext=None, **kwargs):
+        return cls(data, cls.STATUS_ERROR, status_ext, **kwargs)
