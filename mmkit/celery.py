eaeca0c3aaaf143c86b54390d6dd34379e5f04d9
einsfr <einsfr@users.noreply.github.com>
2015-06-08 10:13:02 +0300

    Заготовка для использования Celery

--- mmkit/celery.py
+++ mmkit/celery.py
@@ -1,14 +1,10 @@
 from __future__ import absolute_import
 
-import os
-
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mmkit.conf.dev_local')
-
-from django.conf import settings
+from mmkit.conf import common
 
 app = Celery('mmkit')
 
-app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.config_from_object('celeryconfig')
+app.autodiscover_tasks(lambda: common.INSTALLED_APPS)
