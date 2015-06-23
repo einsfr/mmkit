c74f1428b6661a8a21573ce7ec684262fcd9ce94
einsfr <einsfr@users.noreply.github.com>
2015-06-23 18:45:03 +0300

    Продолжение работы над конвертером

--- efsw/conversion/converter/cli_converter.py
+++ efsw/conversion/converter/cli_converter.py
@@ -1,4 +1,5 @@
-import subprocess, threading
+import subprocess
+import threading
 
 from django.conf import settings
 
@@ -13,11 +14,27 @@ class CliConverter:
                                    'переменной EFSW_FFMPEG_BIN в одном из файлов с настройками или передайте этот '
                                    'путь непосредственно в конструктор с помощью аргумента ffmpeg_bin.')
         self.ffmpeg_bin = ffmpeg_bin
-        self.progress = 0
+        self.duration = None
 
-    def convert(self, args_seq=None):
+    def _calculate_progress(self, current_time):
+        if self.duration is None:
+            raise  # Нужно написать какую-то исключительную ситуацию
+
+    def convert(self, args_seq=None, progress_callback=None):
+        default_args = [self.ffmpeg_bin, '-hide_banner', '-n', '-nostdin']
         if args_seq is None:
-            args = [self.ffmpeg_bin, ]
+            args = default_args
         else:
-            args = args_seq.insert(0, self.ffmpeg_bin)
-        proc = subprocess.Popen(args, stderr=subprocess.PIPE)
+            args = default_args + args_seq
+        proc = subprocess.Popen(args, stderr=subprocess.PIPE, universal_newlines=True)
+        for line in proc.stderr:
+            if self.duration is None:
+                p = line.find('Duration: ')
+                if p >= 0:
+                    self.duration = line[p + 10:p + 21]  # это нужно преобразовать сразу во что-то, чтоб потом считать
+            else:
+                if line[0:6] == 'frame=':
+                    p = line.find('time=')
+                    time = line[p + 5:p + 16]
+                    if progress_callback is not None:
+                        progress_callback(self._calculate_progress(time))
