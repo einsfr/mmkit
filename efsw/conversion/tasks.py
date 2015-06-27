61cda4cc1b723a068ca742d6511893aae97fb698
einsfr <einsfr@users.noreply.github.com>
2015-06-27 17:07:28 +0300

    Продолжение работы

--- efsw/conversion/tasks.py
+++ efsw/conversion/tasks.py
@@ -1,27 +1,103 @@
-import subprocess
+import time
+from threading import Thread
 
 from celery import shared_task
+from celery.exceptions import TimeoutError
+from django.conf import settings
 
 from efsw.conversion.converter.converter import Converter
+from efsw.conversion import default_settings
 
 
-@shared_task(queue='conversion')
-def convert(args_builder):
+class ConvertCallbacksFactory:
 
-    def _start_callback(proc: subprocess.Popen):
-        nonlocal process
-        process = proc
-        print('started')
+    def __init__(self, conv_id):
+        self.conv_id = conv_id
+        self.progress_call_interval = getattr(
+            settings,
+            'EFSW_CONVERTER_PROGRESS_NOTIFY_INTERVAL',
+            default_settings.EFSW_CONVERTER_PROGRESS_NOTIFY_INTERVAL
+        )
+        self.last_progress_call_ts = 0.0
+        self.convert_proc = None
+        self.process_result_timeout = getattr(
+            settings,
+            'EFSW_CONVERTER_PROCESS_PROGRESS_RESULT_TIMEOUT',
+            default_settings.EFSW_CONVERTER_PROCESS_PROGRESS_RESULT_TIMEOUT
+        )
 
-    def _progress_callback(frame):
-        print(frame)
+    def get_start_callback(self):
 
-    def _error_callback(log, conv_exception):
-        pass
+        def _start_callback(proc):
+            self.convert_proc = proc
+            notify_conversion_started.delay(self.conv_id)
 
-    def _success_callback():
-        print('ok')
+        return _start_callback
 
-    process = None
+    def get_progress_callback(self):
+
+        def _process_result(result):
+            try:
+                print(result.wait(timeout=self.process_result_timeout, interval=1))
+            except TimeoutError:
+                print('timeout')
+
+        def _progress_callback(frame):
+            ts = time.time()
+            if ts - self.last_progress_call_ts >= self.progress_call_interval:
+                self.last_progress_call_ts = ts
+                result = notify_conversion_progress.delay(self.conv_id, frame)
+                Thread(target=_process_result, name='conv_result_process', args=(result, )).start()
+
+        return _progress_callback
+
+    def get_success_callback(self):
+
+        def _success_callback():
+            notify_conversion_success.delay(self.conv_id)
+
+        return _success_callback
+
+    def get_error_callback(self):
+
+        def _error_callback(log, conv_exception):
+            error_msg = None
+            notify_conversion_error.delay(self.conv_id, error_msg)
+
+        return _error_callback
+
+    def get_callback_dict(self):
+        return {
+            'start_callback': self.get_start_callback(),
+            'progress_callback': self.get_progress_callback(),
+            'success_callback': self.get_success_callback(),
+            'error_callback': self.get_error_callback()
+        }
+
+
+@shared_task(queue='conversion', ignore_result=True)
+def convert(conv_id, args_builder):
     converter = Converter()
-    converter.convert(args_builder, _start_callback, _progress_callback, _success_callback, _error_callback)
+    cb_factory = ConvertCallbacksFactory(conv_id)
+    converter.convert(args_builder, **cb_factory.get_callback_dict())
+
+
+@shared_task(queue='control', ignore_result=True)
+def notify_conversion_started(conv_id):
+    print('conversion started: {0}'.format(conv_id))
+
+
+@shared_task(queue='control')
+def notify_conversion_progress(conv_id, frame):
+    print('{0} progress: {1}'.format(conv_id, frame))
+    return 'progress received'
+
+
+@shared_task(queue='control', ignore_result=True)
+def notify_conversion_error(conv_id, error_msg):
+    print('convertion error: {0}'.format(conv_id))
+
+
+@shared_task(queue='control', ignore_result=True)
+def notify_conversion_success(conv_id):
+    print('conversion complete: {0}'.format(conv_id))
--- mmkit/conf/common.py
+++ mmkit/conf/common.py
@@ -98,6 +98,12 @@ TEMPLATES = [
     }
 ]
 
+CELERY_ACCEPT_CONTENT = ['pickle']
+
+CELERY_TASK_SERIALIZER = 'pickle'
+
+CELERY_RESULT_SERIALIZER = 'pickle'
+
 CELERY_TIMEZONE = TIME_ZONE
 
 CELERY_DEFAULT_QUEUE = 'control'
@@ -107,6 +113,8 @@ CELERY_DEFAULT_EXCHANGE_TYPE = 'topic'
 CELERY_DEFAULT_ROUTING_KEY = 'control.default'
 
 CELERY_DEFAULT_EXCHANGE = 'default'
+
+CELERY_DISABLE_RATE_LIMITS = True  # Если эта возможность не используется, то лучше её отключить - это улучшает производительность
 
 CELERY_QUEUES = (
     Queue('control', routing_key='control.#'),
